17a58c9f68296fc3750e7a2471d6bbcc1c04cc02
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-03-11 17:04:25 +0530

    Angularized content section

---
+++ control/content/controllers/content.home.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular) {
+  angular
+    .module('customerFeedbackPluginContent')
+    .controller('ContentHomeCtrl', ['$scope',
+      function ($scope) {
+
+      }]);
+})(window.angular);
