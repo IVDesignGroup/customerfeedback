a61666f40518efa894f21f488b2fe0cb743413ea
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-04-21 16:55:01 +0530

    Made date format change to mm/dd/yy

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -139,6 +139,12 @@
               }
           });
 
+          ContentHome.convertDate = function (inputFormat) {
+              function pad(s) { return (s < 10) ? '0' + s : s; }
+              var d = new Date(inputFormat);
+              return [pad(d.getMonth()+1), pad(d.getDate()), d.getFullYear()].join('/');
+          }
+
         /*
          * watch for changes in data and trigger the saveDataWithDelay function on change
          * */
