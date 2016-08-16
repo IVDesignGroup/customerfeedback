641c0ac742c83f77e8b9954437902ab258a5a92c
rishabhdixit <rishabh.dixit@intelligrape.com>
2016-08-16 13:21:03 +0530

    Corrected average rating calculation formula on CP side, #128083781

--- control/content/controllers/content.home.controller.js
+++ control/content/controllers/content.home.controller.js
@@ -156,10 +156,11 @@
                               if (uniqueTokens.indexOf(event.data.userToken) == -1) {
                                   uniqueTokens.push(event.data.userToken);
                                   uniqueReviews.push(event.data);
+                                  ContentHome.avgRating = ((ContentHome.avgRating * ContentHome.totalReviews) + parseInt(event.data.data.starRating))/(ContentHome.totalReviews + 1);
                                   elemCount = elemCount + 1;
                                   ContentHome.totalReviews = elemCount;
-                              }
-                              ContentHome.avgRating = ((ContentHome.avgRating * ContentHome.totalReviews) - event.lastReviewCount + event.data.data.starRating)/ContentHome.totalReviews;
+                              } else
+                                  ContentHome.avgRating = ((ContentHome.avgRating * ContentHome.totalReviews) - event.lastReviewCount + event.data.data.starRating) / ContentHome.totalReviews;
                           }
                           break;
                       default :
